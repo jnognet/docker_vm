1d2031cab09d725b80a4a9fbd5cac03b09256afd
José Neto <jnognet@gmail.com>
2022-11-04 20:27:34 -0500

    corrigindo apache para rodar o code igniter4

--- scripts/019_mysql.sh
+++ scripts/019_mysql.sh
@@ -2,6 +2,7 @@
                                                                                                                        
 set -o xtrace                                                                                                          
                                                                                                                        
+docker container rm -f mysql
 docker container rm -f nginx
 docker container rm -f apache
 docker run --rm -d --name mysql -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=senha mysql:8.0.31                       
